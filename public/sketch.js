8c7e9344787f8daa9e1746564654313b178009fc
Glitch (socketio-chat) <none>
2020-02-11 15:08:14 +0000

    👚✊ Checkpoint ./public/index.html:9838890/20 ./public/sketch.js:9838890/35

--- public/sketch.js
+++ public/sketch.js
@@ -320,7 +320,7 @@ const render = () => {
 };
 render();
 
-const onWindowResize = () => {
+window.onresize = () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
