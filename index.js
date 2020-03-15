e94698a0132e7f61b3cfa65a73e3be087d2d2c9b
Glitch (virtual-exhibition-004) <none>
2020-03-15 16:06:35 +0000

    🌝🚊 Checkpoint ./public/sketch.js:9838890/12190 ./index.js:9838890/561

--- index.js
+++ index.js
@@ -22,47 +22,44 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
 
 let ibase = 0;
-// db.serialize(() => {
-//   if (!exists) {
-//     db.run(
-//       "CREATE TABLE Images (id INTEGER PRIMARY KEY AUTOINCREMENT, base TEXT)"
-//     );
+db.serialize(() => {
+  if (!exists) {
+    db.run(
+      "CREATE TABLE Count (id INTEGER PRIMARY KEY AUTOINCREMENT, base TEXT)"
+    );
 
-//     db.serialize(() => {
-//       for (let i = 0; i < 1; i++) {
-//         db.run(`INSERT INTO Images (base) VALUES ('${ibase}')`);
-//       }
-//     });
-//   } else {
-//     db.each("SELECT * from Images", (err, row) => {
-//       if (row) {
-//         //console.log(`record: ${row.base}`);
-//       }
-//     });
-//   }
-// });
+    db.serialize(() => {
+      for (let i = 0; i < 1; i++) {
+        db.run(`INSERT INTO Count (base) VALUES ('${ibase}')`);
+      }
+    });
+  } else {
+    db.each("SELECT * from Count", (err, row) => {
+      if (row) {
+        //console.log(`record: ${row.base}`);
+      }
+    });
+  }
+});
 
 // Chatroom
 
 var numUsers = 0;
 
-// db.all("SELECT * from Images", (err, rows) => {
-//   for (let i = rows.length - 1; i < rows.length; i++) {
-//     imageHistory.push(rows[i].base);
-//     const s = rows[i].base;
-//     console.log(
-//       i,
-//       s.substring(0, 40) + "..." + s.substring(s.length - 40, s.length)
-//     );
-//   }
-// });
+db.all("SELECT * from Count", (err, rows) => {
+  for (let i = 0; i < rows.length; i++) {
+    const s = rows[i].base;
+    ibase = parseInt(s);
+  }
+});
 
 io.on("connection", function(socket) {
   ibase += 1;
+  socket.emit("visitor count", {num: ibase});
   socket.broadcast.emit("visitor count", {num: ibase});
-  // db.run(`INSERT INTO Images (base) VALUES (?)`, data.base, error => {
-  //   console.log("upload " + (error == undefined ? "success" : error));
-  // });
+  db.run(`UPDATE Count SET (base) = (?)`, ibase, error => {
+    console.log("upload " + (error == undefined ? "success" : error));
+  });
   console.log("new user");
   socket.on("probe", function(data) {
     console.log("probing");
